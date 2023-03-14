5da6caecc374aebee77f06e43753727d266282db
kashifmehmood1991 <45274086+kashifmehmood1991@users.noreply.github.com>
2023-03-14 19:32:35 +0500

    Create cleanup_docker_images.sh

---
+++ cleanup_docker_images.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+docker_images=$(docker images -q)
+all_containers=$(docker ps -aq --format "{{.Names}}")
+for image in $all_containers;
+do
+docker inspect --format '{{.Image}}' $image |cut -d":" -f2 |cut -c 1-12 >>container_images.txt;
+done
+
+for image in $docker_images; do
+  grep $image container_images.txt
+  if [[ $? -ne 0 ]]; then
+    #echo "image to remove $image"
+     docker rmi $image
+  fi
+done
+rm  container_images.txt
